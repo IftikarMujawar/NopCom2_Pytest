3fd70df1c07c21da23c032620951c25064c9d74c
IftikarMujawar <mujawariftikar@gmail.com>
2024-03-17 12:38:38 +0530

    This is NopCom2 Version-1

---
+++ utilities/readconfigfile.py
@@ -0,0 +1,18 @@
+import configparser
+
+config = configparser.RawConfigParser()
+config.read("C:\\IftikaR\\Complete Python\\nopcom_Pytest\\Configuration\\config.ini")
+
+#config.read("..\\Configuration\\config.ini")
+
+class Readconfig:
+    @staticmethod
+    def getEmail():
+        Email = config.get('login data', 'email')
+        return Email
+
+    @staticmethod
+    def getPassword():
+        Password = config.get('login data', 'password')
+        return Password
+
